bd07a495396da2d8f8f8b76b9dd07701c8d25d3e
Benjamin Peterson <benjamin@python.org>
2017-12-15 08:10:58 -0800

    make percona work with openssl 1.1 (#2)
    * make percona work with openssl 1.1
    
    Based on https://github.com/percona/percona-server/commit/dddeabde2fc7f6896347d69a35880ab236498ee3.
    However, we can simplify a bit because we don't care about supporting truly ancient OpenSSL versions.
    
    This can be dropped on upgrade.

--- sql/rpl_slave.cc
+++ sql/rpl_slave.cc
@@ -5002,7 +5002,6 @@ err:
   mysql_mutex_unlock(&mi->run_lock);
   DBUG_LEAVE;                                   // Must match DBUG_ENTER()
   my_thread_end();
-  ERR_remove_state(0);
   pthread_exit(0);
   return(0);                                    // Avoid compiler warnings
 }
@@ -5193,7 +5192,6 @@ err:
   }
 
   my_thread_end();
-  ERR_remove_state(0);
   pthread_exit(0);
   DBUG_RETURN(0); 
 }
@@ -6357,7 +6355,6 @@ llstr(rli->get_group_master_log_pos(), llbuff));
 
   DBUG_LEAVE;                            // Must match DBUG_ENTER()
   my_thread_end();
-  ERR_remove_state(0);
   pthread_exit(0);
   return 0;                             // Avoid compiler warnings
 }
